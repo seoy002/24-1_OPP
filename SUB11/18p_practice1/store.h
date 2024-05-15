4bcf2996582fff8dd3e03d1e2326bf6e48e287b2
seoy002 <163312586+seoy002@users.noreply.github.com>
2024-05-15 12:24:28 +0900

    Create store.h

---
+++ SUB11/18p_practice1/store.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "fruit.h"
+class store {
+public:
+	store() {};
+	store(int a, int b, int c, float d, float e, float f, float g, float h) {
+		apple = fruit(e, a);
+		pear = fruit(f, b);
+		peach = fruit(g, c);
+		mango = fruit(h, d);
+	}
+	fruit apple, pear, peach, mango;
+	float total();
+};
+
