84d54f9e46ab4fb3aadba400823d1a6a8c9397f6
seoy002 <163312586+seoy002@users.noreply.github.com>
2024-05-24 14:59:08 +0900

    Create Engineer.cpp

---
+++ SUB12/9-2_13p/Engineer.cpp
@@ -0,0 +1,10 @@
+#include "Engineer.h"
+int Engineer::getCarPrice(Car* xx) {
+	return xx->price;
+	}
+int Engineer::getWheels(Car* xx) {
+	return xx->wheels;
+}
+int Engineer::getSpeed(Car* xx) {
+	return xx->speed;
+}
